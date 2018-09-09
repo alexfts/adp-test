6ab7e46f11dd05da1cfab3dfc38871b9f229dd7a
alexfts <alxfetisova@gmail.com>
2018-09-09 09:28:55 -0700

    Code cleanup, updates readme

--- src/main.js
+++ src/main.js
@@ -1,192 +1,223 @@
 'use strict';
 
-let helpers = {
-	loadJSON: function(callback) {   
-		var req = new XMLHttpRequest();
-        req.overrideMimeType("application/json");
-    	req.open(
-    		'GET', 
-    		'https://raw.githubusercontent.com/alexfts/adp-test/master/src/quiz.json', 
-    		true
-    	);
-    	req.onreadystatechange = function () {
-    		if (req.readyState == 4 && req.status == "200") {
-            	callback(req.responseText);
-          	}
-    	};
-    	req.send(null);  
- 	}
-};
+(function() {
 
-
-/*
- * Main class
- */
-class Game {
-
-	constructor(quizzes) {
-		this.quizzes = quizzes;
-		this.currentQuiz = quizzes[0]; // FIXIT
-		this.score = 0;
-		this.nextQuestion = 0;
-	}
-
-	gameOver() {
-		return this.nextQuestion >= this.currentQuiz.questions.length;
-	}
-
-	getNextQuestionAndAnswers() {
-		return this.currentQuiz.questions[this.nextQuestion];
-	}
-
-	processAnswer(index) {
-		let answers = this.getNextQuestionAndAnswers().answers;
-		let isCorrect = answers[index].value;
-		if (isCorrect) {
-			this.score++;
-		}
-		this.nextQuestion++;
-		return isCorrect;
-	}
-
-}
-
-class GameHandler {
-	constructor() {
-		helpers.loadJSON(response => {
-   			let quizzes = JSON.parse(response).quizzes;
-   			this.game = new Game(quizzes);
-   			this.view = new GameView(this);
-   		});
-	}
-
-	showNextQuestionAndScore() {
-		if (!this.game.gameOver()) {
-			this.view.displayNextQuestionAndScore(
-				this.game.getNextQuestionAndAnswers(),
-				this.game.score
+ /*
+  * Helper class to load JSON
+  */
+	class Helpers {
+		static loadJSON(callback) {   
+			var req = new XMLHttpRequest();
+			req.overrideMimeType("application/json");
+			req.open(
+				'GET',
+				'https://raw.githubusercontent.com/alexfts/adp-test/master/src/quiz.json', 
+				true
 			);
-		} else {
-			this.finishGame();
-		}
+			req.onreadystatechange = function () {
+				if (req.readyState == 4 && req.status == "200") {
+					callback(req.responseText);
+				}
+			};
+			req.send(null);  
+	 	}
 	}
 
-	handleSelectedAnswer(option) {
-		let id = parseInt(option.id);
-		let isCorrect = this.game.processAnswer(id);
-		if (isCorrect) this.view.updateScore(this.game.score);
-		this.view.showAnswerFeedback(option, isCorrect);
-		setTimeout(this.showNextQuestionAndScore.bind(this), 2000);
-	}
+	/*
+	 * Model for the quiz game; stores quizzes and the game state
+	 */
+	class Game {
 
-	startGame(quizButton) {
-		let quizTitle = quizButton.innerText;
-		this.game.currentQuiz = this.game.quizzes.filter(
-			quiz => quiz.title === quizTitle
-		)[0];
-		this.view.showGameView();
-		this.showNextQuestionAndScore();
-	}
-
-	finishGame() {
-		let pass = this.game.score / this.game.currentQuiz.questions.length > 0.5;
-		this.view.showScoreView(
-			this.game.score,
-			this.game.currentQuiz.questions.length,
-			pass
-		);
-	}
-
-	get quizNames() {
-		return this.game.quizzes.map(({title}) => title);
-	}
-
-}
-
-class GameView {
-	constructor(handler) {
-		this.handler = handler;
-		this.welcomeView = document.getElementById("welcomeView");
-		this.quizView = document.getElementById("quizView");
-		this.scoreView = document.getElementById("scoreView");
-		this.showWelcomeView();
-		this.setupEventListeners();
-	}
-
-	showWelcomeView() {
-		this.welcomeView.style.display = "block";
-		let selectQuizContainer = document.getElementById('selectQuizContainer');
-		for (let quizName of this.handler.quizNames) {
-			let quizButton = document.createElement('button');
-			quizButton.className = 'selectQuizButton';
-			quizButton.innerHTML = quizName;
-			selectQuizContainer.appendChild(quizButton);
+		constructor(quizzes) {
+			this.quizzes = quizzes;
+			this.selectedQuizQuestions = [];
+			this.score = 0;
+			this.currentQuestionIndex = 0;
 		}
 
-	}
-
-	showGameView() {
-		this.quizView.style.display = "block";
-		this.welcomeView.style.display = "none";
-	}
-
-	showScoreView(score, total, pass) {
-		this.quizView.style.display = "none";
-		this.scoreView.style.display = "block";
-		let finalScoreMessage = document.getElementById("finalScore");
-		finalScoreMessage.innerHTML = `You scored: ${score} out of ${total}`;
-
-		let didPassMessage = document.getElementById("didPassMessage");
-		didPassMessage.innerHTML = pass ? 
-			"Congratulations!" : ":( Next time!";
-	}
-
-	displayNextQuestionAndScore(questionAndAnswers, score) {
-		let question = document.getElementById("question");
-		question.innerHTML = questionAndAnswers.question;
-
-		questionAndAnswers.answers.forEach((answer, i) => {
-			let button = document.getElementById(`${i}`);
-			button.innerHTML = answer.content;
-			button.style.backgroundColor = "#f5f4ff";
-		});
-
-		this.updateScore(score);
-	}
-
-	showAnswerFeedback(button, isCorrect) {
-		if (isCorrect) {
-			button.style.backgroundColor = "#cefcba";
-		} else {
-			button.style.backgroundColor = "#fcabab";
+		get isGameOver() {
+			return this.currentQuestionIndex >= this.selectedQuizQuestions.length;
 		}
-	}
 
-	setupEventListeners() {
-		let answerOptionsContainer = document.getElementById("answerOptions");
-		answerOptionsContainer.addEventListener('click', e => {
-			let clickedOption = e.target;
-			if (clickedOption.className === "optionButton") {
-				this.handler.handleSelectedAnswer(clickedOption);
+		get currentQuestion() {
+			return this.selectedQuizQuestions[this.currentQuestionIndex].question;
+		}
+
+		get currentAnswerOptions() {
+			return this.selectedQuizQuestions[this.currentQuestionIndex].answers;
+		}
+
+		processAnswer(index) {
+			let answers = this.currentAnswerOptions;
+			let isCorrect = answers[index].value;
+
+			if (isCorrect) {
+				this.score++;
 			}
-		});
+			this.currentQuestionIndex++;
+			return isCorrect;
+		}
 
-		let selectQuizContainer = document.getElementById("selectQuizContainer");
-		selectQuizContainer.addEventListener('click', e => {
-			let selectedQuizButton = e.target;
-			if (selectedQuizButton.className === "selectQuizButton") {
-				this.handler.startGame(selectedQuizButton);
+		get didWin() {
+			return this.score / this.selectedQuizQuestions.length > 0.5;
+		}
+
+	}
+
+	/*
+	 * Controller for the quiz game
+	 */
+	class GameHandler {
+
+		constructor() {
+			Helpers.loadJSON(response => {
+	   			let quizzes = JSON.parse(response).quizzes;
+	   			this.game = new Game(quizzes);
+	   			this.view = new GameView(this);
+	   		});
+		}
+
+		startGame(quizButton) {
+			let quizTitle = quizButton.innerText;
+			this.game.selectedQuizQuestions = this.game.quizzes.filter(
+				quiz => quiz.title === quizTitle
+			)[0].questions;
+
+			this.view.showGameView();
+			this.showNextQuestion();
+		}
+
+		showNextQuestion() {
+			if (!this.game.isGameOver) {
+				this.view.displayNextQuestionAndScore(
+					this.game.currentQuestion,
+					this.game.currentAnswerOptions,
+					this.game.score
+				);
+
+			} else {
+				this.finishGame();
 			}
-		});
+		}
+
+		handleAnswerClick(option) {
+			let id = parseInt(option.id);
+			let isCorrect = this.game.processAnswer(id);
+			if (isCorrect) this.view.updateScore(this.game.score);
+
+			this.view.showAnswerFeedback(option, isCorrect);
+			setTimeout(this.showNextQuestion.bind(this), 2000);
+		}
+
+		finishGame() {
+			let pass = this.game.didWin;
+			this.view.showScoreView(
+				this.game.score,
+				this.game.selectedQuizQuestions.length,
+				pass
+			);
+		}
+
+		get quizNames() {
+			return this.game.quizzes.map(({title}) => title);
+		}
+
 	}
 
-	updateScore(score) {
-		document.getElementById("score").innerHTML = `Score: ${score}`;
-	}
-}
+ /*
+  * View class for the quiz game; manipulates the DOM
+  */
+	class GameView {
 
-let gameHandler;
-window.addEventListener('load', () => {
-	gameHandler = new GameHandler();
-});
+		constructor(handler) {
+			this.handler = handler;
+			this.welcomeView = document.getElementById("welcomeView");
+			this.quizView = document.getElementById("quizView");
+			this.scoreView = document.getElementById("scoreView");
+
+			this.setupEventListeners();
+			this.showWelcomeView();
+		}
+
+		showWelcomeView() {
+			this.welcomeView.style.display = "block";
+			
+			let selectQuizContainer = document.getElementById('selectQuizContainer');
+			for (let quizName of this.handler.quizNames) {
+				let quizButton = document.createElement('button');
+				quizButton.className = 'selectQuizButton';
+				quizButton.innerHTML = quizName;
+				selectQuizContainer.appendChild(quizButton);
+			}
+		}
+
+		showGameView() {
+			this.quizView.style.display = "block";
+			this.welcomeView.style.display = "none";
+
+			this.answerButtons = document.querySelectorAll(".optionButton");
+		}
+
+		showScoreView(score, total, pass) {
+			this.quizView.style.display = "none";
+			this.scoreView.style.display = "block";
+
+			let finalScoreMessage = document.getElementById("finalScore");
+			finalScoreMessage.innerHTML = `You scored: ${score} out of ${total}`;
+
+			let didPassMessage = document.getElementById("didPassMessage");
+			didPassMessage.innerHTML = pass ? 
+				"Congratulations!" : ":( Next time!";
+		}
+
+		displayNextQuestionAndScore(currentQuestion, answers, score) {
+			let question = document.getElementById("question");
+			question.innerHTML = currentQuestion;
+
+			answers.forEach((answer, i) => {
+				let button = this.answerButtons[i];
+				button.innerHTML = answer.content;
+				button.style.backgroundColor = "#f5f4ff";
+			});
+
+			this.updateScore(score);
+		}
+
+		updateScore(score) {
+			document.getElementById("score").innerHTML = `Score: ${score}`;
+		}
+
+		showAnswerFeedback(button, isCorrect) {
+			if (isCorrect) {
+				button.style.backgroundColor = "#cefcba";
+			} else {
+				button.style.backgroundColor = "#fcabab";
+			}
+		}
+
+		setupEventListeners() {
+			let answerOptionsContainer = document.getElementById("answerOptions");
+			answerOptionsContainer.addEventListener('click', e => {
+				let clickedAnswer = e.target;
+				if (clickedAnswer.className === "optionButton") {
+					this.handler.handleAnswerClick(clickedAnswer);
+				}
+			});
+
+			let selectQuizContainer = document.getElementById("selectQuizContainer");
+			selectQuizContainer.addEventListener('click', e => {
+				let selectedQuizButton = e.target;
+				if (selectedQuizButton.className === "selectQuizButton") {
+					this.handler.startGame(selectedQuizButton);
+				}
+			});
+		}
+	}
+
+	let gameHandler;
+	window.addEventListener('load', () => {
+		gameHandler = new GameHandler();
+	});
+
+})();
 
